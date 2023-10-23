7242dd14d6ddfbdf9e059d6ef7e06740f1aea7e4
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2023-10-23 21:46:19 +0600

    updated main2.js

--- main2.js
+++ main2.js
@@ -6,6 +6,43 @@ $(document).ready(function(){
             "Dhaka": ["Dhaka", "Gazipur", "Narayanganj"],
             "Chittagong": ["Chittagong", "Cox's Bazar", "Comilla"],
             // Add more states and cities as needed
+        },
+        "United States": {
+            "Select States":["Select City" ],
+            "Alabama-us": ["Montgomery	", "Huntsville"],
+            "Alaska-us": ["Anchorage", "Fairbanks", "Knik-Fairview"],
+            "Montgomery-us": ["Riverside", "Centerville", "Fairview"],
+            "California-us": ["Los Angeles", "San Diego", "San Jose", "Bakersfield"],
+            "Sacramento-us": ["Riverside", "Centerville", "Fairview"],
+            "Phoenix-us": ["Anchorage", "Fairbanks", "Knik-Fairview"],
+            "Arizona-us": ["Anchorage", "Fairbanks", "Knik-Fairview"],
+            "Alaska-us": ["Anchorage", "Fairbanks", "Knik-Fairview"],
+            // Add more states and cities as needed
+        },
+        "India": {
+            "Andhra Pradesh": ["Adoni","Dowlaiswaram", "Amaravati", "Chandragiri"],
+            "Bihar": ["Chandigarh", "Barauni", "Begusarai","Bettiah", "Bihar Sharif", "Begusarai","Bodh Gaya"],
+            "Chandigarh": ["Ara", "Barauni", "Begusarai","Bettiah", "Bihar Sharif", "Begusarai","Bodh Gaya"],
+            "Chhattisgarh": ["Ambikapur", "Bhilai", "Bilaspur","Bettiah", "Bihar Sharif", "Begusarai","Bodh Gaya"],
+            "Delhi": ["Delhi", "New Delhi", "Begusarai","Bettiah", "Bihar Sharif", "Begusarai","Bodh Gaya"],
+            "Goa": ["Madgaon", "Panaji", "Begusarai","Bettiah", "Bihar Sharif", "Begusarai","Bodh Gaya"],
+            // Add more states and cities as needed
+        },
+
+        "Bangladesh": {
+            "Dhaka": ["Dhaka", "Gazipur", "Narayanganj"],
+            "Chittagong": ["Chittagong", "Cox's Bazar", "Comilla"],
+            // Add more states and cities as needed
+        },
+        "Bangladesh": {
+            "Dhaka": ["Dhaka", "Gazipur", "Narayanganj"],
+            "Chittagong": ["Chittagong", "Cox's Bazar", "Comilla"],
+            // Add more states and cities as needed
+        },
+        "Bangladesh": {
+            "Dhaka": ["Dhaka", "Gazipur", "Narayanganj"],
+            "Chittagong": ["Chittagong", "Cox's Bazar", "Comilla"],
+            // Add more states and cities as needed
         }
     };
 
@@ -18,28 +55,28 @@ $(document).ready(function(){
     $('.country').change(function(){
         var selectedCountry = $(this).val();
         var states = data[selectedCountry];
-        $('#state').empty(); // Clear old options
+        $('.state').empty(); // Clear old options
 
         for (var state in states) {
-            $('#state').append('<option value="'+state+'">'+state+'</option>');
+            $('.state').append('<option value="'+state+'">'+state+'</option>');
         }
 
         // Trigger state change event to update city dropdown
-        $('#state').change();
+        $('.state').change();
     });
 
     // When state selection changes, update city dropdown
-    $('#state').change(function(){
-        var selectedCountry = $('#country').val();
+    $('.state').change(function(){
+        var selectedCountry = $('.country').val();
         var selectedState = $(this).val();
         var cities = data[selectedCountry][selectedState];
-        $('#city').empty(); // Clear old options
+        $('.city').empty(); // Clear old options
 
         for (var i = 0; i < cities.length; i++) {
-            $('#city').append('<option value="'+cities[i]+'">'+cities[i]+'</option>');
+            $('.city').append('<option value="'+cities[i]+'">'+cities[i]+'</option>');
         }
     });
 
     // Trigger country change event to initialize state and city dropdowns
-    $('#country').change();
+    $('.country').change();
 });
